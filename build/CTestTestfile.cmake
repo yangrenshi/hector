6b7f9d9eae318045fe7251a8f8b7951851749b1e
yangrenshi <3171668917@qq.com>
2023-03-01 22:00:00 +0800

    1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yang/map/src
+# Build directory: /home/yang/map/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("nav_demo")
---
+++ build/nav_demo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yang/map/src/nav_demo
+# Build directory: /home/yang/map/build/nav_demo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
